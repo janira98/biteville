bba3689259133ecb3fd194e2941eab12066dae60
it19046808@my.sllit.lk <janirajayaprabha@gmail.com>
2023-07-04 04:38:30 +0530

    update

--- app/SignUp/page.jsx
+++ app/SignUp/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
       <div className='flex justify-center'>
-        <div className='bg-white flex flex-col justify-center items-center min-w-[400px] drop-shadow-mealShadow my-8 p-[20px] gap-[70px]'>
+        <div className='bg-white flex flex-col justify-center items-center min-w-[400px] drop-shadow-mealShadow my-8 p-[20px] gap-[70px] rounded-[16px]'>
           <h2 className='font-bold text-[39px] '>Sign Up</h2>
           <div className='flex flex-col w-full gap-[25px]'>
             <input type="text" placeholder='Enter your phone number' className='border-solid border-brandRed border-2 rounded-[8px] p-[8px] focus:border-brandRed focus:focus:outline-none ' />
